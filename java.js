fd4e49380252a435b7f6fa00656fc5d8e717100d
Murad Alkhatib <84707872+MoradAlkhatib@users.noreply.github.com>
2021-05-30 16:05:11 +0300

    Update java.js

---
+++ java.js
@@ -0,0 +1,15 @@
+var name =prompt("please write Cat here And Check the C capital");
+
+while(name!="Cat"){
+
+prompt("please write Cat here And Check the C capital");
+
+}
+
+
+var numberOfCat= prompt("Please Enter The Number Of Your Cats");
+
+for(var i=0;i<numberOfCat;i++)
+{
+  document.write('<img src="http://lofrev.net/wp-content/photos/2014/09/Cat-logo.gif">')
+}

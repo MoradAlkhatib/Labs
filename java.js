a2845688c6a1ec58e9988c65b0adb6fb5221930b
Murad Alkhatib <84707872+MoradAlkhatib@users.noreply.github.com>
2021-05-31 21:41:03 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,15 +1,51 @@
-var name =prompt("please write Cat here And Check the C capital");
 
-while(name!="Cat"){
 
-name=prompt("please write Cat here And Check the C capital");
+
+	
+function Name (){
+  
+ let name=prompt("Enter Your firstName Please ---Admin is murad");
+ let age=prompt("Enter Your Age Please ----Admin is 24");
+  if(name=="murad" && age==24)
+  {
+    alert("Admins Here");
+
+  }
+  else{
+    alert("Hello "+name+" You Are Welcome");
+  }
 
 }
 
+Name();
 
-var numberOfCat= prompt("Please Enter The Number Of Your Cats");
 
-for(var i=0;i<numberOfCat;i++)
-{
-  document.write('<img src="http://lofrev.net/wp-content/photos/2014/09/Cat-logo.gif">')
-}
+
+
+
+
+
+
+
+
+
+
+//var name =prompt("please write Cat here And Check the C capital");
+
+//while(name!="Cat"){
+
+//name =prompt("please write Cat here And Check the C capital");
+
+
+//}
+
+
+// var numberOfCat= prompt("Please Enter The Number Of Your Cats");
+
+
+
+
+// for(var i=0;i<numberOfCat;i++)
+//{
+  //document.write('<img src="http://lofrev.net/wp-content/photos/2014/09/Cat-logo.gif">')
+//}
